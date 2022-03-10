035c68bc7b8e6777b723d4c5a989468fee193acc
visiky <xinming.lxj@antgroup.com>
2022-03-10 14:09:33 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+/** @type {import('gatsby').GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+      title: ``,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "",
+      "spaceId": ""
+    }
+  }, "gatsby-plugin-styled-components", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "G-HWKMMN50JG"
+    }
+  }, "gatsby-plugin-react-helmet", "gatsby-transformer-remark", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
